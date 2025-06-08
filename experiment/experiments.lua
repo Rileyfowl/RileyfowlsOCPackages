d4dd45668c236dd8c085fd29bb1d1151aa130a00
Rileyfowl <rileyfowl@gmail.com>
2025-06-08 16:17:49 +0200

    Changed to glueball exp

--- experiment/experiments.lua
+++ experiment/experiments.lua
@@ -1,9 +1,10 @@
-function dump(o)
+--prints table to string
+function Dump(o)
    if type(o) == 'table' then
       local s = '{ '
       for k,v in pairs(o) do
          if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
+         s = s .. '['..k..'] = ' .. Dump(v) .. ','
       end
       return s .. '} '
    else
@@ -18,7 +19,7 @@ Experiment = {
     devices={},
     --key = NAME_OF_DEVICE, value = {energy = ENERGY_PERCENTAGE, port = BEAM_PORT}
     settings = {}
-}   
+}
 
 function Experiment:new (o)
       o = o or {}   -- create object if user does not provide one
@@ -52,7 +53,7 @@ function Experiment:add_setting(name, energy_percentage, beam_port)
 end
 
 --create fusion experiment
-fusion_exp = Experiment:new{name = "fusion", info="power generation"}
+local fusion_exp = Experiment:new{name = "fusion", info="power generation"}
 
 --add the primary connected accelerator component
 local component = require("component")
@@ -63,7 +64,9 @@ for i in next, fusion_exp.devices do
     print(i)
 end
 
-fusion_exp:add_setting("la1", 5,3)
+--set only the energy percentage
+fusion_exp:add_setting("la1", 50)
 
-print("fusion_exp settings", dump(fusion_exp.settings))
+-- dump all settings to console
+print("fusion_exp settings", Dump(fusion_exp.settings))
 
